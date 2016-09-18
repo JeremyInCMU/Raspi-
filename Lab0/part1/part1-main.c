63827d9f850b2e05d1e1c92f52c59da9248e1faf
JeremyInCMU <jiamingx90@gmail.com>
2016-09-18 21:42:19 +0000

    update

--- Lab0/part1/part1-main.c
+++ Lab0/part1/part1-main.c
@@ -9,7 +9,7 @@ extern void strTable(char *s, char *d, int sl, int dl);
 int main()
 {
    /* Assume that dst[i] is always >= 23 (decimal) */
-   char src[] = "theinitialstring\n";
+   char src[] = "theinitialstring";
    char dst[] = "PENGUINS";
    int i, srcLen, dstLen; 
    
